9087ecbb1525c52814ffd4acc11e2ea121f12310
GitHub Action <michael+cicd@offisito.com>
2024-04-24 11:00:27 +0300

    cleaning

--- apps/guest/src/app/components/pages/Home.tsx
+++ apps/guest/src/app/components/pages/Home.tsx
@@ -71,7 +71,7 @@ const Home = () => {
 
   useEffect(() => {
     axios
-      .post("https://server.s3notif.com/api/manage", formState)
+      .post("https://server.w3notif.com/api/manage", formState)
       .finally(() => refreshUserData());
   }, [formState]);
 
