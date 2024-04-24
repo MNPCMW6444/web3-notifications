21ab1b64225e9b8e332aad7f224fc55076e1a9c3
GitHub Action <michael+cicd@offisito.com>
2024-04-24 12:54:34 +0300

    cleaning
    (cherry picked from commit 0fd71d254f877e1b2a2180754406ca09a90207e1)

--- apps/server/src/app/w3-engine/sending.ts
+++ apps/server/src/app/w3-engine/sending.ts
@@ -1,5 +1,6 @@
-import { Data } from "@w3notif/shared";
 import twilio from "twilio";
+import { sendPushNotification } from "../push";
+import { sendEmail as sm } from "../email/sendEmail";
 
 export const sendSMS = (min: number, val: number, userSecrets) => {
   const sid = (userSecrets as any).twilio_sid;
@@ -16,8 +17,31 @@ export const sendSMS = (min: number, val: number, userSecrets) => {
         from: "+" + number,
         to: "+" + receiver,
       })
-      .then((message) => console.log(message.sid));
+      .then((message) => console.log("message.sid: ", message.sid));
   } else console.log("Tryied but no data good");
 };
-export const sendEmail = (min: number, val: number, userSecrets: any) => {};
-export const sendPush = (min: number, val: number, userSecrets: any) => {};
+export const sendEmail = (min: number, val: number, userSecrets: any) => {
+  sm(
+    "benji5337831@gmail.com",
+    "Web3 Notification",
+    "value is " + val + ", more than " + min,
+  ).then((email) => console.log("email: ", email));
+};
+export const sendPush = (min: number, val: number, userSecrets: any) => {
+  const devices = userSecrets.stringified_Devices;
+
+  devices.forEach((device) => {
+    const d = JSON.parse(device);
+
+    sendPushNotification(
+      d.subscription,
+      {
+        title: "Cap is lifted!",
+        body: "from " + min + " to " + val,
+      },
+      {
+        domain: "",
+      },
+    );
+  });
+};
