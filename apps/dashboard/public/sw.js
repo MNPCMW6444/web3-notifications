7637522da1ffb836ab21ee52ec4db2d7bdee4a5e
Michael Nusair <mnpcmw6444@gmail.com>
2024-08-30 23:13:40 +0300

    calssic

--- apps/dashboard/public/sw.js
+++ apps/dashboard/public/sw.js
@@ -1,16 +1,15 @@
-let currentVersion = "default-version";
+let currentVersion = 'default-version';
 const defaultCacheName = `v${currentVersion}`;
-const urlsToCache = ["/", "/index.html", "/styles/main.css", "/script/main.js"];
 
-self.addEventListener("install", (event) => {
+/*self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(defaultCacheName).then((cache) => {
       return cache.addAll(urlsToCache);
     }),
   );
-});
+});*/
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
@@ -18,14 +17,14 @@ self.addEventListener("fetch", (event) => {
   );
 });
 
-self.addEventListener("message", (event) => {
-  if (event.data.action === "setVersion") {
+self.addEventListener('message', (event) => {
+  if (event.data.action === 'setVersion') {
     currentVersion = event.data.version;
     console.log(`Version set to ${currentVersion}`);
   }
 });
 
-self.addEventListener("push", function (event) {
+self.addEventListener('push', function (event) {
   const data = event.data.json();
   const options = {
     body: data.body,
@@ -34,15 +33,15 @@ self.addEventListener("push", function (event) {
   event.waitUntil(self.registration.showNotification(data.title, options));
 });
 
-self.addEventListener("notificationclick", function (event) {
+self.addEventListener('notificationclick', function (event) {
   const notificationData = event.notification.data;
   event.notification.close();
   event.waitUntil(
-    clients.matchAll({ type: "window" }).then((windowClients) => {
+    clients.matchAll({ type: 'window' }).then((windowClients) => {
       const chatUrl = `${notificationData.domain}/notification?data=${encodeURIComponent(JSON.stringify(notificationData))}`;
       for (var i = 0; i < windowClients?.length; i++) {
         var client = windowClients[i];
-        if (client.url.startsWith(chatUrl) && "focus" in client) {
+        if (client.url.startsWith(chatUrl) && 'focus' in client) {
           return client.focus();
         }
       }
