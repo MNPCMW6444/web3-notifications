4df972c5d3b851dbc66a842611ae46bb0961fe93
Michael Nusair <mnpcmw6444@gmail.com>
2024-08-30 18:48:06 +0300

    fixes

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,14 +1,14 @@
 /// <reference types='vitest' />
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import {nxViteTsPaths} from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/dashboard',
 
   server: {
-    port: 4100,
+    port: 3500,
     host: 'localhost',
   },
 
