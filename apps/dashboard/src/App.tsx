edd925560c9586396becfe2cf9ee757e06a62ae4
Michael Nusair <mnpcmw6444@gmail.com>
2024-08-30 23:07:52 +0300

    good

--- apps/dashboard/src/App.tsx
+++ apps/dashboard/src/App.tsx
@@ -1,9 +1,16 @@
-import { AuthPage } from '@the-libs/base-frontend';
+import { Typography } from '@mui/material';
+import { AuthContextProvider } from '@the-libs/base-frontend';
+
+const MainMessage = ({ text }: { text: string }) => (
+  <Typography>{text}</Typography>
+);
 
 const App = () => {
-  const AuthContextProivder = Auth;
-
-  return <AuthPage></AuthPage>;
+  return (
+    <AuthContextProvider MainMessage={MainMessage}>
+      <Typography>asdasdas</Typography>
+    </AuthContextProvider>
+  );
 };
 
 export default App;
