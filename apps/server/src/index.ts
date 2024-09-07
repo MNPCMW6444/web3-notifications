7acbc8ccd17507f5dba975a8710a75b3c21bd362
Michael Nusair <mnpcmw6444@gmail.com>
2024-09-07 16:46:02 +0300

    will do in cluster

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -10,6 +10,9 @@ const avveURL =
 const swapURL =
   'http://swap:80/?chain=ethereum&from=0xdac17f958d2ee523a2206206994597c13d831ec7&tab=swap&to=0x4c9edd5852cd905f086c759e8383e09bff1e68b3';
 
-goSomeWhereAndWait(avveURL);
+setTimeout(() => {
+  console.log('60 passed');
+  goSomeWhereAndWait(avveURL);
 
-goSomeWhereAndWait(swapURL);
+  goSomeWhereAndWait(swapURL);
+}, 60000);
