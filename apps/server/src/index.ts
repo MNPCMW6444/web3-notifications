244b6fbe4546002891fe017779f53afb44d74336
GitHub Action <michael+cicd@offisito.com>
2024-09-07 13:40:52 +0300

    cool

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,6 +4,5 @@ import { apiRouter } from './new/apiRouter';
 startMongoAndExpress(apiRouter, [], [], true, true)
 
 
-fetch("http://aave.w3:80/").then((x)=>console.log(x)).catch((x)=>console.log(x))
 
 
