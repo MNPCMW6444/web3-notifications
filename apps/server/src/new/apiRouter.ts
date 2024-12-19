8042e217c00c3dca48e0354e7c8594bb380369f7
mnpcmw6444@gmail.com <michael+cicd@offisito.com>
2024-12-19 22:29:07 +0100

    22

--- apps/server/src/new/apiRouter.ts
+++ apps/server/src/new/apiRouter.ts
@@ -21,7 +21,7 @@ const makeCall = async (
   to,
   url = 'https://web-3-notifications-8775.twil.io/path_1',
   cb = async () => {},
-  forSeconds = 15,
+  forSeconds = 22,
 ) => {
   const client = twilio(process.env.T_I, process.env.T_S);
 
